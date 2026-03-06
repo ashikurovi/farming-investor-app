93c47dce77f3063283e5be434cc4988afd1899c0
Ashikur Rahman Ovi <ashikurovi2003@gmail.com>
2026-03-06 14:47:10 +0600

    update

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: "https://farming-investor-api.vercel.app",
+    // http://localhost:8000
     prepareHeaders: (headers) => {
       if (typeof window !== "undefined") {
         const token = localStorage.getItem("token");
